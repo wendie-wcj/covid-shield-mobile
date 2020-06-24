64b63c16b3544dd788f3d20ab5245bfba837d140
Nick Mallory <nick.mallory@ontario.ca>
2020-06-24 09:29:36 -0400

    Added code button to region not covered

--- src/screens/home/views/NoExposureUncoveredRegionView.tsx
+++ src/screens/home/views/NoExposureUncoveredRegionView.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import {Text} from 'components';
+import React, {useCallback} from 'react';
+import {Text, ButtonMultiline} from 'components';
 import {useI18n} from '@shopify/react-i18n';
 
 import {BaseHomeView} from '../components/BaseHomeView';
+import {useNavigation} from '@react-navigation/native';
 
 export const NoExposureUncoveredRegionView = () => {
   const [i18n] = useI18n();
+  const navigation = useNavigation();
+  const onDiagnosed = useCallback(() => navigation.navigate('DataSharing'), [navigation]);
 
   return (
     // note you can add an icon i.e. <BaseHomeView iconName="icon-offline>
@@ -16,6 +19,13 @@ export const NoExposureUncoveredRegionView = () => {
       <Text variant="bodyText" color="bodyText" marginBottom="l">
         {i18n.translate('Home.NoExposureDetected.RegionNotCovered.Body')}
       </Text>
+      <ButtonMultiline
+        text={i18n.translate('Home.NoExposureDetected.RegionNotCovered.DiagnosedBtnText1')}
+        text1={i18n.translate('Home.NoExposureDetected.RegionNotCovered.DiagnosedBtnText2')}
+        variant="bigFlat"
+        internalLink
+        onPress={onDiagnosed}
+      />
     </BaseHomeView>
   );
 };
